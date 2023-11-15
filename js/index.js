6efec12f910bd1a7cd592b4ef371b61d771acddb
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2023-11-15 13:19:33 -0500

    vale

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 let hamburger=document.getElementById("hamburger-menu")
-
 let information=document.getElementById("information")
+let items=document.getElementById("items")
+let  arriba = document.getElementById("arriba")
+let opciones = document.getElementById("items")
+let abajo = document.getElementById("abajo")
+let cerrar = document.getElementById("cerrar")
+cerrar.addEventListener("click", ()=>{
+    alert("lolo")
+})
+
+abajo.addEventListener("click", ()=>{
+    if (opciones.classList.contains("itemnull")) {
+        opciones.classList.remove("itemnull")
+        opciones.classList.add("item")
+    }
+})
+arriba.addEventListener("click", () =>{
+    if (opciones.classList.contains("item")) {
+        opciones.classList.remove("item")
+        opciones.classList.add("itemnull")
+    }
+})
 
 hamburger.addEventListener("click", function(){
     if (information.classList.contains("box-null")) {
